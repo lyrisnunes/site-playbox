0e8d319108004909cd2481e4450a41198e4df72b
lyrisnunes~ <lyrisnunesdasilva@gmail.com>
2023-01-24 01:02:52 -0300

    Alteração imagem

--- script.js
+++ script.js
@@ -21,4 +21,19 @@
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  breakpoints:{
+    0: {
+      slidesPerView:1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    }
+  }
 });
